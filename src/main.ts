3841432c52d7cedcf4aa9a41545f4025ee40dfae
SeamMiner <niatit130@gmail.com>
2022-01-12 01:22:33 +0300

    Add Delay to Tooltips

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,20 @@ import store from "./store";
 import i18n from "./i18n";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 /* @ts-ignore:disable-next-line */
-import { VTooltip } from "v-tooltip";
+import VTooltip from "v-tooltip";
 
 createApp(App)
   .use(i18n)
   .use(store)
   .use(router)
-  .directive("tooltip", VTooltip)
+  .use(VTooltip, {
+    themes: {
+      tooltip: {
+        delay: {
+          show: 3000,
+          hide: 0,
+        }
+      }
+    }
+  })
   .mount("#app");
