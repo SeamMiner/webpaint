c18ffe47f4b247442bd3a5ebbc634994d677d081
SeamMiner <niatit130@gmail.com>
2022-01-18 14:36:16 +0300

    Add Lint

--- src/paint/main.ts
+++ src/paint/main.ts
@@ -1,11 +1,12 @@
 export class Paint {
   private _canvas: HTMLCanvasElement;
 
-  private _memCanvas: HTMLCanvasElement = document.createElement('canvas');
+  private _memCanvas: HTMLCanvasElement = document.createElement("canvas");
 
   private _ctx: CanvasRenderingContext2D | null;
 
-  private _memCtx: CanvasRenderingContext2D | null = this._memCanvas.getContext('2d');
+  private _memCtx: CanvasRenderingContext2D | null =
+    this._memCanvas.getContext("2d");
 
   private _coords: { x: number; y: number };
 
@@ -73,8 +74,8 @@ export class Paint {
     this._canvas.height =
       this._scale * (this._canvas.parentElement?.clientHeight || 0);
 
-    this._memCanvas.width = this._canvas.width
-    this._memCanvas.height = this._canvas.height
+    this._memCanvas.width = this._canvas.width;
+    this._memCanvas.height = this._canvas.height;
     this.resize();
   }
 
@@ -94,7 +95,7 @@ export class Paint {
         this._canvas.scrollTop) *
       this._scale;
 
-    this._points.push(Object.assign({}, this._coords))
+    this._points.push(Object.assign({}, this._coords));
   }
   public start(event: PointerEvent) {
     if (this._currentButton == event.button) {
@@ -117,14 +118,14 @@ export class Paint {
         this._lineCap == "eraser" ? this._lineWidth * 4 : this._lineWidth;
       this._ctx!.lineCap = this._lineCap == "eraser" ? "round" : this._lineCap;
       this._ctx!.strokeStyle =
-      this._lineCap == "eraser"
-        ? "#000"
-        : this.ConvertRGBtoHex(
-            parseInt(red),
-            parseInt(green),
-            parseInt(blue),
-            this._opacity
-          );
+        this._lineCap == "eraser"
+          ? "#000"
+          : this.ConvertRGBtoHex(
+              parseInt(red),
+              parseInt(green),
+              parseInt(blue),
+              this._opacity
+            );
 
       document.addEventListener("pointermove", this._drawHandler);
       this.reposition(event);
@@ -132,36 +133,41 @@ export class Paint {
   }
   public stop(event: PointerEvent | MouseEvent) {
     if (this._currentButton === event.button) {
-      this.updateCanvas(this._canvas, this._memCtx)
+      this.updateCanvas(this._canvas, this._memCtx);
 
       document.removeEventListener("pointermove", this._drawHandler);
       this._currentButton = null;
-      this._points = []
+      this._points = [];
     }
   }
   public draw(event: PointerEvent) {
-    this.updateCanvas(this._memCanvas, this._ctx)
+    this.updateCanvas(this._memCanvas, this._ctx);
 
     this.reposition(event);
 
     if (this._points.length < 6) {
-        const b = this._points[0];
-        this._ctx!.beginPath();
-        this._ctx!.arc(b.x, b.y, this._ctx!.lineWidth / 2, 0, Math.PI * 2, !0);
-        this._ctx!.closePath();
-        this._ctx!.fill();
-        return
+      const b = this._points[0];
+      this._ctx!.beginPath();
+      this._ctx!.arc(b.x, b.y, this._ctx!.lineWidth / 2, 0, Math.PI * 2, !0);
+      this._ctx!.closePath();
+      this._ctx!.fill();
+      return;
     }
     this._ctx!.beginPath();
     this._ctx!.moveTo(this._points[0].x, this._points[0].y);
     // draw a bunch of quadratics, using the average of two points as the control point
     for (let i = 1; i < this._points.length - 2; i++) {
-        const c = (this._points[i].x + this._points[i + 1].x) / 2,
-            d = (this._points[i].y + this._points[i + 1].y) / 2;
-        this._ctx!.quadraticCurveTo(this._points[i].x, this._points[i].y, c, d)
+      const c = (this._points[i].x + this._points[i + 1].x) / 2,
+        d = (this._points[i].y + this._points[i + 1].y) / 2;
+      this._ctx!.quadraticCurveTo(this._points[i].x, this._points[i].y, c, d);
     }
-    this._ctx!.quadraticCurveTo(this._points[this._points.length - 2].x, this._points[this._points.length - 2].y, this._points[this._points.length - 1].x, this._points[this._points.length - 1].y);
-    this._ctx!.stroke()
+    this._ctx!.quadraticCurveTo(
+      this._points[this._points.length - 2].x,
+      this._points[this._points.length - 2].y,
+      this._points[this._points.length - 1].x,
+      this._points[this._points.length - 1].y
+    );
+    this._ctx!.stroke();
   }
   public eraseAll(ctx = this._ctx) {
     ctx?.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -174,15 +180,18 @@ export class Paint {
     history.push(this._memCanvas.toDataURL());
   }
 
-  private restoreState(history = this._undoHistory, saveTo = this._redoHistory) {
+  private restoreState(
+    history = this._undoHistory,
+    saveTo = this._redoHistory
+  ) {
     if (history.length) {
       this.saveState(saveTo, true);
       const img = new Image();
       img.src = history.pop()!;
       img.onload = () => {
         this.eraseAll(this._memCtx);
-        this._memCtx?.drawImage(img,0,0,);
-        this.updateCanvas(this._memCanvas, this._ctx)
+        this._memCtx?.drawImage(img, 0, 0);
+        this.updateCanvas(this._memCanvas, this._ctx);
       };
     }
     console.log(this._undoHistory, this._redoHistory);
@@ -211,13 +220,13 @@ export class Paint {
   }
 
   public clear() {
-    this.eraseAll(this._ctx)
-    this.eraseAll(this._memCtx)
-    this.saveState(this._undoHistory)
+    this.eraseAll(this._ctx);
+    this.eraseAll(this._memCtx);
+    this.saveState(this._undoHistory);
   }
 
   public updateCanvas(canvas = this._canvas, ctx = this._ctx) {
     this.eraseAll(ctx);
-    ctx!.drawImage(canvas, 0,0) 
+    ctx!.drawImage(canvas, 0, 0);
   }
 }
