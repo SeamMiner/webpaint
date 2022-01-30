bb355d72f2c34b1e9f0f0ca5f2af34a9c6284eb2
SeamMiner <niatit130@gmail.com>
2022-01-30 19:25:58 +0300

    Fix Eraser Bug

--- src/paint/main.ts
+++ src/paint/main.ts
@@ -228,8 +228,13 @@ export class Paint {
   }
 
   public updateCanvas(canvas = this._canvas, ctx = this._ctx) {
+    const globalCompositeOperation = ctx!.globalCompositeOperation
+    ctx!.globalCompositeOperation = 'source-over'
+
     this.eraseAll(ctx);
     ctx!.drawImage(canvas, 0, 0);
+
+    ctx!.globalCompositeOperation = globalCompositeOperation
   }
 
   public updateCanvasProperties(ctx = this._ctx) {
